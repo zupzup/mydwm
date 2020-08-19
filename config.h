3228cf0f442ecda520910c257b5f9be7483a3ae7
Mario Zupan <mario@zupzup.org>
2020-08-19 20:54:57 +0200

    add zoom

--- config.h
+++ config.h
@@ -37,6 +37,7 @@ static const Rule rules[] = {
 	{ "Notable",  NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Postman",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "Tresorit",  NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Zoom",  NULL,          NULL,       1 << 4,       0,           -1 },
 	{ NULL,  NULL,            "Timeular", 1 << 6,       0,           -1 },
 };
 
